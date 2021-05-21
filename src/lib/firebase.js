cf7c2206891bd7a86c5a71f8c513138e60569d9d
NguyenTaiThao <thaogreenhouses@gmail.com>
2021-05-21 17:24:30 +0700

    Use Firebase fix config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,13 @@ import firebase from "firebase";
 
 if (!firebase.apps.length) {
   const firebaseConfig = {
-    apiKey: "AIzaSyBNfjjw4sBG9O_hhmm7D5w2V6PT39KdJxU",
-    authDomain: "fir-sample-3e6fe.firebaseapp.com",
-    projectId: "fir-sample-3e6fe",
-    storageBucket: "fir-sample-3e6fe.appspot.com",
-    messagingSenderId: "1020733071963",
-    appId: "1:1020733071963:web:7a96b8caf7247badebd57e",
+    apiKey: "AIzaSyBQmnPb5z1k1M3g54lnMgfP580m3AxSCeI",
+    authDomain: "test-fb-5d568.firebaseapp.com",
+    projectId: "test-fb-5d568",
+    storageBucket: "test-fb-5d568.appspot.com",
+    messagingSenderId: "153302387030",
+    appId: "1:153302387030:web:0405f5b946612787cfaef7",
+    measurementId: "G-5H0XDWE1X8",
   };
 
   firebase.initializeApp(firebaseConfig);
